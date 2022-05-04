a3f46eddf59f061c8e90d25e6481d223b5b7b7d7
Marco Brasini <marco.brasini@studio.unibo.it>
2022-05-04 10:56:49 +0200

    UMDLattice __init__ basis inversion tested.

--- test_UMDLattice.py
+++ test_UMDLattice.py
@@ -26,3 +26,23 @@ def test_UMDLattice_init_assignement():
     assert Lattice.atoms == atoms
     assert np.array_equal(Lattice.dirBasis, basis)
     assert np.array_equal(Lattice.invBasis, np.linalg.inv(basis))
+
+
+def test_UMDLattice_init_basis_inversion():
+    """
+    Test the __init__ function matrix inversion operation for a non singular
+    matrix of lattice vectors. The matrix product between dirBasis and invBasis
+    must be equal (or close) to the identical matrix.
+
+    """
+    name = 'LatticeName'
+    basis = np.array([[2, 1, -3],
+                      [-1, 0, 0],
+                      [-4, 2, 1]])
+    atoms = {'X': 10, 'Y': 2}
+    Lattice = UMDLattice(name, basis, atoms)
+    assert np.allclose(Lattice.dirBasis@Lattice.invBasis, np.identity(3))
+
+
+test_UMDLattice_init_assignement()
+test_UMDLattice_init_basis_inversion()
