13a5587df0f18dab6a864e8492e6d1be743b9f94
Marco Brasini <marco.brasini@studio.unibo.it>
2022-05-04 10:40:55 +0200

    UMDLattice __init__ function assignement tested.

---
+++ test_UMDLattice.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 10:23:02 2022
+
+@author: marco
+"""
+
+from UMDLattice import UMDLattice
+
+import numpy as np
+
+
+def test_UMDLattice_init_assignement():
+    """
+    Test the __init__ function assignement operations with a non singular
+    matrix of lattice vectors.
+
+    """
+    name = 'LatticeName'
+    basis = np.array([[2, 1, -3],
+                      [-1, 0, 0],
+                      [-4, 2, 1]])
+    atoms = {'X': 10, 'Y': 2}
+    Lattice = UMDLattice(name, basis, atoms)
+    assert Lattice.name == name
+    assert Lattice.atoms == atoms
+    assert np.array_equal(Lattice.dirBasis, basis)
+    assert np.array_equal(Lattice.invBasis, np.linalg.inv(basis))
