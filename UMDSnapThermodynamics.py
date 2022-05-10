422b1941bf043afe2892ba2028985bedcc6534ff
Marco Brasini <marco.brasini@studio.unibo.it>
2022-05-10 16:02:58 +0200

    UMDSnapThermodynamics class tested.

--- UMDSnapThermodynamics.py
+++ UMDSnapThermodynamics.py
@@ -9,20 +9,59 @@ Created on Tue May 10 15:40:29 2022
 class UMDSnapThermodynamics:
     """
     Class UMDSnapThermodynamics to contain the thermodynamic data of a single
-    MD simulation snapshot.
+    molecular dynamics simulation snapshot.
 
     """
 
-    def __init__(self, temperature, pressure, energy):
+    def __init__(self, temperature=0.0, pressure=0.0, energy=0.0):
+        """
+        Construct UMDSnapThermodynamics object.
+
+        Parameters
+        ----------
+        temperature : float, optional
+            System temperature. The default is 0.0.
+        pressure : float, optional
+            System pressure. The default is 0.0.
+        energy : float, optional
+            System energy. The default is 0.0.
+
+        Returns
+        -------
+        UMDSnapThermodynamics object.
+
+        """
         self.temperature = temperature
         self.pressure = pressure
         self.energy = energy
 
     def __str__(self):
+        """
+        Overload of the str function.
+
+        Returns
+        -------
+        string : string
+            Report of the thermodynamics values of the snapshot.
+
+        """
         string  = "Temperature = " + str(self.temperature) + ' K\n'
         string += "Pressure = " + str(self.pressure) + ' GPa\n'
         string += "Energy = " + str(self.energy) + ' eV'
         return string
 
     def save(self, outfile):
+        """
+        Print on file the UMDSnapThermodynamics informations.
+
+        Parameters
+        ----------
+        outfile : output file
+            The output file where to print the UMDSnapThermodynamics.
+
+        Returns
+        -------
+        None.
+
+        """
         outfile.write(str(self)+'\n')
---
+++ test_UMDSnapThermodynamics.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 15:45:55 2022
+
+@author: marco
+"""
+
+from UMDSnapThermodynamics import UMDSnapThermodynamics
+
+
+# %% UMDSnapThermodynamics __init__ function tests
+def test_UMDSnapThermodynamics_init_default():
+    """
+    Test the __init__ function defualt constructor.
+
+    """
+    snapthermodynamics = UMDSnapThermodynamics()
+    assert snapthermodynamics.temperature == 0.0
+    assert snapthermodynamics.pressure == 0.0
+    assert snapthermodynamics.energy == 0.0
+
+
+def test_UMDSnapThermodynamics_init_assignement():
+    """
+    Test the __init__ function assignement operations.
+
+    """
+    temperature = 1400  # in K
+    pressure = 20       # in GPa
+    energy = -1050      # in eV
+    snapthermodynamics = UMDSnapThermodynamics(temperature, pressure, energy)
+    assert snapthermodynamics.temperature == temperature
+    assert snapthermodynamics.pressure == pressure
+    assert snapthermodynamics.energy == energy
+
+
+test_UMDSnapThermodynamics_init_default()
+test_UMDSnapThermodynamics_init_assignement()
+
+
+# %% UMDSnapThermodynamics __str__ function tests
+def test_UMDSnapThermodynamics_init_assignement():
+    """
+    Test the __str__ function to convert the UMDSnapThermodynamics information
+    into a descriptive and printable string object.
+
+    """
+    temperature = 1400  # in K
+    pressure = 23       # in GPa
+    energy = -1050      # in eV
+    snapthermodynamics = UMDSnapThermodynamics(temperature, pressure, energy)
+    string  = "Temperature = 1400 K\n"
+    string += "Pressure = 23 GPa\n"
+    string += "Energy = -1050 eV"
+    assert str(snapthermodynamics) == string
+
+
+test_UMDSnapThermodynamics_init_assignement()
