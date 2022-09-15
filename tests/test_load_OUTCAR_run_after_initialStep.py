e4cb30192423d7044f98141a81c6f550fa259e14
Marco Brasini <marco.brasini@studio.unibo.it>
2022-09-15 20:42:34 +0200

    load_OUTCAR functions _run_before_initialStep, _run_around_initialStep, _run_after_initialStep tested.

---
+++ tests/test_load_OUTCAR_run_after_initialStep.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 20:30:51 2022
+
+@author: marco
+"""
+
+
+from ..load_OUTCAR import Load_OUTCAR 
+
+import os
+import numpy as np
+import unittest.mock as mock
+
+from ..libs.UMDAtom import UMDAtom
+from ..libs.UMDLattice import UMDLattice
+from ..libs.UMDSimulation import UMDSimulation
+from ..libs.UMDSimulationRun import UMDSimulationRun
+from ..libs.UMDSnapshot import UMDSnapshot
+
+
+class TestLoad_OUTCAR_run_after_initialStep:
+    """
+    Test the _run_after_initialStep function in the Load_OUTCAR class.
+
+    """
+    lattice_name = '2bccH2O+1Fe'
+    H = UMDAtom(name='H', mass=1.00, valence=1.0)
+    O = UMDAtom(name='O', mass=16.00, valence=6.0)
+    Fe = UMDAtom(name='Fe', mass=55.85, valence=8.0)
+    atoms = {O: 15, H: 28, Fe: 1}
+    basis = 5.7*np.identity(3)
+    lattice = UMDLattice(lattice_name, basis, atoms)
+
+    @mock.patch.object(UMDSnapshot, 'save')
+    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar')
+    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar_null')
+    def test_run_after_initialStep_single(self,  mock_null, mock_load,
+                                          mock_save):
+        """
+        Test _run_after_initialStep function executing the UMDSnapshot method
+        UMDSnapshot_from_outcar as many times as the number of steps in the
+        current simulation run. The usual UMDSnapshot_from_outcar_null is never
+        called.
+        For an OUTCAR file containing a single simulation run, the function
+        call operates over all the simulation steps.
+
+        """
+        Load_OUTCAR.reset()
+        Load_OUTCAR.finalStep = 300
+        run0 = UMDSimulationRun(0, 300, 0.5)
+        simulation = UMDSimulation('', self.lattice, [run0])
+        with open('./examples/OUTCAR_single.outcar', 'r') as outcar:
+            with open('./examples/OUTCAR_single.umd', 'w') as umd:
+                Load_OUTCAR._run_after_initialStep(outcar, umd, simulation)
+                assert mock_null.call_count == 0
+                assert mock_load.call_count == 300
+                assert mock_save.call_count == 300
+                umd.close()
+            outcar.close()
+        # After the _simulation_after_initialStep call the number of steps
+        # in the last UMDSimulationRun must be 300.
+        assert simulation.steps() == 300
+        os.remove('./examples/OUTCAR_single.umd')
+
+    @mock.patch.object(UMDSnapshot, 'save')
+    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar')
+    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar_null')
+    def test_run_after_initialStep_multiple(self, mock_null, mock_load,
+                                            mock_save):
+        """
+        Test _run_after_initialStep function executing the UMDSnapshot method
+        UMDSnapshot_from_outcar as many times as the number of steps in the
+        current simulation run. The usual UMDSnapshot_from_outcar_null is never
+        called.
+        For an OUTCAR file containing multiple simulation runs concatenated,
+        the function call operates only over the last simulation run.
+
+        """
+        Load_OUTCAR.reset()
+        run0 = UMDSimulationRun(0, 300, 0.5)
+        run1 = UMDSimulationRun(1, 600, 0.5)
+        run2 = UMDSimulationRun(2, 1000, 0.4)
+        simulation = UMDSimulation('', self.lattice)
+        with open('./examples/OUTCAR_multiple.outcar', 'r') as outcar:
+            with open('./examples/OUTCAR_multiple.umd', 'w') as umd:
+                # apply the _simulation_before_initialStep to load the run 0
+                simulation.runs.append(run0)
+                Load_OUTCAR.finalStep = 300
+                Load_OUTCAR._run_after_initialStep(outcar, umd, simulation)
+                assert mock_null.call_count == 0
+                assert mock_load.call_count == 300
+                assert mock_save.call_count == 300
+                assert simulation.runs[0].steps == 300
+                # apply the _simulation_before_initialStep to load the run 1
+                simulation.runs.append(run1)
+                Load_OUTCAR.loadedSteps = 300
+                Load_OUTCAR.finalStep = 900
+                Load_OUTCAR._run_after_initialStep(outcar, umd, simulation)
+                assert mock_null.call_count == 0
+                assert mock_load.call_count == 900
+                assert mock_save.call_count == 900
+                assert simulation.runs[1].steps == 600
+                # apply the _simulation_before_initialStep to load the run 2
+                simulation.runs.append(run2)
+                Load_OUTCAR.loadedSteps = 900
+                Load_OUTCAR.finalStep = 1900
+                Load_OUTCAR._run_after_initialStep(outcar, umd, simulation)
+                assert mock_null.call_count == 0
+                assert mock_load.call_count == 1900
+                assert mock_save.call_count == 1900
+                assert simulation.runs[2].steps == 1000
+                umd.close()
+            outcar.close()
+        # After the multiple _simulation_before_initialStep calls the total
+        # number of steps in the total UMDSimulation must be 0.
+        assert simulation.steps() == 1900
+        os.remove('./examples/OUTCAR_multiple.umd')
--- tests/test_load_OUTCAR_run_before_initialStep.py
+++ tests/test_load_OUTCAR_run_before_initialStep.py
@@ -6,9 +6,8 @@ Created on Wed Sep 14 17:19:32 2022
 """
 
 
-from ..load_UMDSimulationRun import Load_OUTCAR 
+from ..load_OUTCAR import Load_OUTCAR
 
-import os
 import numpy as np
 import unittest.mock as mock
 
@@ -19,8 +18,11 @@ from ..libs.UMDSimulationRun import UMDSimulationRun
 from ..libs.UMDSnapshot import UMDSnapshot
 
 
-class Test_simulation_before_initialStep:
+class TestLoad_OUTCAR_run_before_initialStep:
+    """
+    Test the _run_before_initialStep function in the Load_OUTCAR class.
 
+    """
     lattice_name = '2bccH2O+1Fe'
     H = UMDAtom(name='H', mass=1.00, valence=1.0)
     O = UMDAtom(name='O', mass=16.00, valence=6.0)
@@ -29,65 +31,65 @@ class Test_simulation_before_initialStep:
     basis = 5.7*np.identity(3)
     lattice = UMDLattice(lattice_name, basis, atoms)
 
-    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar_null')
     @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar')
-    def test_simulation_before_initialStep_single(self, mock, mock_null):
+    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar_null')
+    def test_run_before_initialStep_single(self, mock_null, mock_load):
         """
-        Test _simulation_before_initialStep function executing the UMDSnapshot
-        method UMDSnapshot_from_outcar_null as many times as the number of
-        steps in the current simulation run. The usual UMDSnapshot_from_outcar
-        is never called.
+        Test _run_before_initialStep function executing the UMDSnapshot method
+        UMDSnapshot_from_outcar_null as many times as the number of steps in
+        the current simulation run. The usual UMDSnapshot_from_outcar is never
+        called.
         For an OUTCAR file containing a single simulation run, the function
         call operates over all the simulation steps.
 
         """
-        Load_OUTCAR.reset_parameters()
+        Load_OUTCAR.reset()
         run0 = UMDSimulationRun(0, 300, 0.5)
         simulation = UMDSimulation('', self.lattice, [run0])
-        with open('./examples/OUTCAR_single.outcar') as outcar:
+        with open('./examples/OUTCAR_single.outcar', 'r') as outcar:
             Load_OUTCAR._run_before_initialStep(outcar, simulation)
-            assert mock.call_count == 0
             assert mock_null.call_count == 300
+            assert mock_load.call_count == 0
             outcar.close()
         # After the _simulation_before_initialStep call the number of steps
         # in the last UMDSimulationRun must be 0.
         assert simulation.steps() == 0
 
-    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar_null')
     @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar')
-    def test_simulation_before_initialStep_multiple(self, mock, mock_null):
+    @mock.patch.object(UMDSnapshot, 'UMDSnapshot_from_outcar_null')
+    def test_run_before_initialStep_multiple(self, mock_null, mock_load):
         """
-        Test _simulation_before_initialStep function executing the UMDSnapshot
-        method UMDSnapshot_from_outcar_null as many times as the number of
-        steps in the current simulation run. The usual UMDSnapshot_from_outcar
-        is never called.
+        Test _run_before_initialStep function executing the UMDSnapshot method
+        UMDSnapshot_from_outcar_null as many times as the number of steps in
+        the current simulation run. The usual UMDSnapshot_from_outcar is never
+        called.
         For an OUTCAR file containing multiple simulation runs concatenated,
         the function call operates only over the last simulation run.
 
         """
-        Load_OUTCAR.reset_parameters()
+        Load_OUTCAR.reset()
         run0 = UMDSimulationRun(0, 300, 0.5)
         run1 = UMDSimulationRun(1, 600, 0.5)
         run2 = UMDSimulationRun(2, 1000, 0.4)
         simulation = UMDSimulation('', self.lattice)
-        with open('./examples/OUTCAR_multiple.outcar') as outcar:
+        with open('./examples/OUTCAR_multiple.outcar', 'r') as outcar:
             # apply the _simulation_before_initialStep to load the run 0
             simulation.runs.append(run0)
             Load_OUTCAR._run_before_initialStep(outcar, simulation)
-            assert mock.call_count == 0
             assert mock_null.call_count == 300
+            assert mock_load.call_count == 0
             assert simulation.runs[0].steps == 0
             # apply the _simulation_before_initialStep to load the run 1
             simulation.runs.append(run1)
             Load_OUTCAR._run_before_initialStep(outcar, simulation)
-            assert mock.call_count == 0
             assert mock_null.call_count == 900
+            assert mock_load.call_count == 0
             assert simulation.runs[1].steps == 0
             # apply the _simulation_before_initialStep to load the run 2
             simulation.runs.append(run2)
             Load_OUTCAR._run_before_initialStep(outcar, simulation)
-            assert mock.call_count == 0
             assert mock_null.call_count == 1900
+            assert mock_load.call_count == 0
             assert simulation.runs[2].steps == 0
             outcar.close()
         # After the multiple _simulation_before_initialStep calls the total
