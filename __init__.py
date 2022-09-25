43d462513c985322b3ece5e573e0a04ff679c8d3
Marco Brasini <marco.brasini@studio.unibo.it>
2022-09-25 18:56:39 +0200

    __init__ package updated.

--- __init__.py
+++ __init__.py
@@ -5,3 +5,10 @@ Created on Wed May 18 23:34:51 2022
 @author: marco
 """
 
+
+from .UMDVaspParser import UMDVaspParser
+
+from .libs.UMDAtom import UMDAtom
+from .libs.UMDLattice import UMDLattice
+from .libs.UMDSnapshot import UMDSnapshot
+from .libs.UMDSimulation import UMDSimulation
